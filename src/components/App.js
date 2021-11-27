f7ffd1a748ea588b7fdf4be188f49898c2ae3ca9
Steven Mather <stevenrmather@gmail.com>
2021-11-26 16:02:52 -0800

    Begeinings of a weather app

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+import './App.css';
+import WeatherNow from './Weather/WeatherNow';
+
+class App extends Component {
+ constructor(props){
+   super(props);
+   this.state ={
+     mosesLake:{
+       lat:'47.130',
+       lon:'-119.27'
+     }
+   };
+ }
+  
+render(){
+  return (
+    <div className="App">
+      <h1 className="App-header">Weather</h1>
+      <div className="grid">
+        <WeatherNow location={this.state.mosesLake}/>
+      </div>
+    </div>
+  );
+}
+
+}
+export default App;
